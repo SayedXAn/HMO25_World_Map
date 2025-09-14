1f122f3cd77eeb40f8514166b9b0a4c362caba16
SayedXAn <106014255+SayedXAn@users.noreply.github.com>
2025-09-14 15:57:48 +0600

    Fully Functioning Mechanism with Dynamic Video Content
    Changed content type to Video
    Implemented single object content display
    Implemented LoadLocalVideo method to avoid rebuilding to change content

--- HMO25 - World Map/Assets/Script/Manager.cs
+++ HMO25 - World Map/Assets/Script/Manager.cs
@@ -1,23 +1,36 @@
 using UnityEngine;
 using UnityEngine.UI;
+using System.IO;
+using UnityEngine.Video;
 
 public class Manager : MonoBehaviour
 {
-    public Image content;
-    public Sprite[] contentImages;
+    public VideoPlayer content;
+    public RenderTexture rt;
+    public string[] videoURLs;
     void Start()
     {
-        
-    }
-
-    void Update()
-    {
-        
+        GetVideosFromLocal();
     }
 
     public void LocationButton(int buttonID)
     { 
-        content.sprite = contentImages[buttonID];
+        content.url = videoURLs[buttonID];
+        rt.Release();
         content.gameObject.SetActive(true);
+        content.Play();
+    }
+
+    public void GetVideosFromLocal()
+    {               
+        for (int i = 0; i < videoURLs.Length; i++)
+        {
+            string avDir = $"{Application.dataPath}/Videos/{i + 1}.mp4";
+            if (File.Exists(avDir))
+            {
+                videoURLs[i] = avDir;
+                Debug.Log("Video "+ (i+1) + " loaded");
+            }
+        }
     }
 }
