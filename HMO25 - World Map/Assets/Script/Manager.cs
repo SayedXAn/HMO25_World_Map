b438221e1e124c845534cbe727c70c82c233e2f6
SayedXAn <106014255+SayedXAn@users.noreply.github.com>
2025-09-14 17:00:36 +0600

    Added content on off smoother with fade animation
    Content and close button now has a fade in/out effect for better visual experience

--- HMO25 - World Map/Assets/Script/Manager.cs
+++ HMO25 - World Map/Assets/Script/Manager.cs
@@ -1,26 +1,62 @@
 using UnityEngine;
-using UnityEngine.UI;
 using System.IO;
 using UnityEngine.Video;
+using DG.Tweening;
+using UnityEngine.UI;
+using System.Collections;
 
 public class Manager : MonoBehaviour
 {
     public VideoPlayer content;
     public RenderTexture rt;
     public string[] videoURLs;
+    public GameObject closeButton;
     void Start()
     {
         GetVideosFromLocal();
+        DOTween.Init();
     }
 
     public void LocationButton(int buttonID)
     { 
         content.url = videoURLs[buttonID];
         rt.Release();
-        content.gameObject.SetActive(true);
+        content.gameObject.SetActive(true);        
+        content.GetComponent<RawImage>().DOFade(1f, 0.5f);
+        closeButton.SetActive(true);
+        closeButton.GetComponent<Image>().DOFade(0.5f, 0.5f);
         content.Play();
+        StartCoroutine(isVideoPlaying());
     }
 
+    IEnumerator isVideoPlaying()
+    {
+        yield return new WaitForSeconds(0.5f);
+        if(content.isPlaying)
+        {
+            StartCoroutine(isVideoPlaying());
+        }
+        else
+        {
+            StopCoroutine(isVideoPlaying());
+        }
+    }
+
+    public void VideoEnded()
+    {
+        
+        content.GetComponent<RawImage>().DOFade(0f, 0.5f);      
+        closeButton.GetComponent<Image>().DOFade(0f, 0.5f);      
+        StartCoroutine(TurnOnOffContent());
+    }
+    
+    IEnumerator TurnOnOffContent()
+    {
+        yield return new WaitForSeconds(0.6f);
+        content.gameObject.SetActive(false);
+        closeButton.SetActive(false);
+        rt.Release();
+    }
     public void GetVideosFromLocal()
     {               
         for (int i = 0; i < videoURLs.Length; i++)
